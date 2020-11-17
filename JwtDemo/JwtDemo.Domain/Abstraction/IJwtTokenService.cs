c0ff58081fe6f76b2a25df227793002fa4c6d43d
VovaMishchenia <volodymyr77m@gmail.com>
2020-11-17 20:43:20 +0200

    first

---
+++ JwtDemo/JwtDemo.Domain/Abstraction/IJwtTokenService.cs
@@ -0,0 +1,14 @@
+﻿
+using JwtDemo.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace JwtDemo.Domain.Abstraction
+{
+    public interface IJwtTokenService
+    {
+        string CreateToken(User user);
+    }
+}
